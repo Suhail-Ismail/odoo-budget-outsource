75ce898d68f7e7663de8a717de26b0911e90a98d
Marc Philippe de Villeres <mpdevilleres@gmail.com>
2018-05-08 15:30:47 +0400

    fix creation of summary from invoice

--- models/invoice_inherit.py
+++ models/invoice_inherit.py
@@ -6,13 +6,13 @@ from odoo import models, fields, api
 class Invoice(models.Model):
     _inherit = 'budget.invoice.invoice'
 
-
     # BASIC FIELDS
     # ----------------------------------------------------------
     # TODO DEPRECATED
     is_resource = fields.Boolean('Is Resource', default=False)
 
     is_outsource = fields.Boolean('Is Resource', default=False)
+
     # outsource_actual_hour = fields.Float(default=0.00)
     # outsource_claim_hour = fields.Float(default=0.00)
 
@@ -20,3 +20,10 @@ class Invoice(models.Model):
     # ----------------------------------------------------------
     # mobilize_id = fields.Many2one('budget.outsource.mobilize')
     # position_id = fields.Many2one('budget.outsource.position')
+
+    # REDIRECT/OPEN OTHER VIEWS BUTTONS
+    # ----------------------------------------------------------
+    def summary_wizard(self):
+        res = super(Invoice, self).summary_wizard()
+        res['context']['is_outsource'] = self.is_outsource
+        return res
